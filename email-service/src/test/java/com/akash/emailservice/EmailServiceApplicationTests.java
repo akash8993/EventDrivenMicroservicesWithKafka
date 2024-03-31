4bb389c2288b0f6b9875b0ae81fe93c3fab6854d
Akash Gupta <gr8akash1994@gmail.com>
2024-03-31 22:34:18 +0530

    First commit for event driver microservices

---
+++ email-service/src/test/java/com/akash/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
